29d21672a8ad3bbe53f2b8bd906dd2651baa30ad
Arttu Viljakainen <arttu.viljakainen@gmail.com>
2019-08-17 19:33:58 +0300

    Updates

---
+++ precache-manifest.51186371c86e88f66d74b98a6968fcb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6536cc1ed5dc08c48841178b922f2e",
+    "url": "/react-training-2019-08/index.html"
+  },
+  {
+    "revision": "7cab7c2d653c86b6af3a",
+    "url": "/react-training-2019-08/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ba4645b30a60203c416a",
+    "url": "/react-training-2019-08/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7cab7c2d653c86b6af3a",
+    "url": "/react-training-2019-08/static/js/2.70909932.chunk.js"
+  },
+  {
+    "revision": "ba4645b30a60203c416a",
+    "url": "/react-training-2019-08/static/js/main.4a87d795.chunk.js"
+  },
+  {
+    "revision": "f50e568ac0806b22c246",
+    "url": "/react-training-2019-08/static/js/runtime~main.5e6940ff.js"
+  }
+]);
