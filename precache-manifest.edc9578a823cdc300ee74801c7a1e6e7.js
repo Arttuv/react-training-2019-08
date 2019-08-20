8a1a1fbfb9097851296aab67a2ae9e7e46dad782
Arttu Viljakainen <arttu.viljakainen@gmail.com>
2019-08-20 20:48:49 +0300

    Updates

--- precache-manifest.edc9578a823cdc300ee74801c7a1e6e7.js
+++ precache-manifest.edc9578a823cdc300ee74801c7a1e6e7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "abdda60060dbe4a26b735850a51767fc",
+    "revision": "b5d7bde5059d7cffb407822bbb375c4f",
     "url": "/react-training-2019-08/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-training-2019-08/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "68fb7294cb169fef9234",
+    "revision": "55a7301ce7df28f207e8",
     "url": "/react-training-2019-08/static/css/main.c23ee2eb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-training-2019-08/static/js/2.4a005ab2.chunk.js"
   },
   {
-    "revision": "68fb7294cb169fef9234",
-    "url": "/react-training-2019-08/static/js/main.3c85b00d.chunk.js"
+    "revision": "55a7301ce7df28f207e8",
+    "url": "/react-training-2019-08/static/js/main.d7783547.chunk.js"
   },
   {
     "revision": "f50e568ac0806b22c246",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-training-2019-08/precache-manifest.92c03f1d227fdb6cebb04566d0c853c2.js"
+  "/react-training-2019-08/precache-manifest.edc9578a823cdc300ee74801c7a1e6e7.js"
 );
 
 self.addEventListener('message', (event) => {
