d6e8b28acc4a1ea8e77f89910781e68a871d3fae
Arttu Viljakainen <arttu.viljakainen@gmail.com>
2019-08-20 22:25:14 +0300

    Updates

--- precache-manifest.a0685388af7777db9be97d02407cea02.js
+++ precache-manifest.a0685388af7777db9be97d02407cea02.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "085212be80fb454ac919f3cf0086c14d",
+    "revision": "e54724f2c61f99e252d1872afff25253",
     "url": "/react-training-2019-08/index.html"
   },
   {
-    "revision": "ecc584577fadfebf55b2",
+    "revision": "f7b6cc1efa258bd21098",
     "url": "/react-training-2019-08/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "07d2cb54c358fc711343",
+    "revision": "cd94e9b3771e4a0bd290",
     "url": "/react-training-2019-08/static/css/main.3b3e1e42.chunk.css"
   },
   {
-    "revision": "ecc584577fadfebf55b2",
-    "url": "/react-training-2019-08/static/js/2.4a005ab2.chunk.js"
+    "revision": "f7b6cc1efa258bd21098",
+    "url": "/react-training-2019-08/static/js/2.dd404843.chunk.js"
   },
   {
-    "revision": "07d2cb54c358fc711343",
-    "url": "/react-training-2019-08/static/js/main.e338aa58.chunk.js"
+    "revision": "cd94e9b3771e4a0bd290",
+    "url": "/react-training-2019-08/static/js/main.162d6e92.chunk.js"
   },
   {
     "revision": "f50e568ac0806b22c246",
