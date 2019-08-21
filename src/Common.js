38e4e3006d0c9b3cf1b22510050eebfc024cd486
Arttu Viljakainen <arttu.viljakainen@gmail.com>
2019-08-21 20:58:03 +0300

    Removed unused code.

--- src/Common.js
+++ src/Common.js
@@ -8,36 +8,6 @@ import "./Common.css";
  */
 function TopNavbar(props) {
   
-    /*return (
-        <nav id="main-navigation" className="navbar navbar-expand fixed-top shadow-sm">
-        <a className="navbar-brand" href="#">PhotoApp</a>
-  
-          <div>
-  
-          {(props.activePage) ? 
-          <ul className="navbar-nav">
-  
-              {props.activePage==='photos' ? 
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Photos <span className="sr-only">(current)</span></a>
-                </li>
-              : 
-              <li className="nav-item">
-                    <a className="nav-link" href="#">Photos <span className="sr-only">(current)</span></a>
-                </li>
-              }
-  
-              {props.activePage==='photo' ? 
-                <li className="nav-item active">
-                    <a className="nav-link" href={window.location.href}>Photo Details<span className="sr-only">(current)</span></a>
-                </li>
-              : null}
-            </ul>
-           : null}
-  
-          </div>  
-        </nav>
-      );*/
     return (
         <Navbar id="main-navigation" variant="light" fixed="top">
             <Container>
